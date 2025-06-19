1d2cc814ed5a4bd122acb2140740c6735ebfcd8a
Daniil <dan.developer@born2be.media>
2025-06-19 10:34:42 +0300

    [feature/meta]: Meta Tags (#4)

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from 'next';
+
 import {
   AboutUs,
   Advantages,
@@ -7,6 +9,25 @@ import {
   Reviews,
   Services,
 } from './components';
+
+export const metadata: Metadata = {
+  title: 'Комплексне просування бренду у соцмережах',
+  description:
+    'Створюємо контент, запускаємо рекламу і будуємо стратегії для вашого бренду у Facebook, Instagram, LinkedIn та TikTok.',
+  openGraph: {
+    title: 'Комплексне просування бренду у соцмережах',
+    description:
+      'Створюємо контент, запускаємо рекламу і будуємо стратегії для вашого бренду у Facebook, Instagram, LinkedIn та TikTok.',
+    images: 'https://mysmmexperts.com/images/meta.png',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Комплексне просування бренду у соцмережах',
+    description:
+      'Створюємо контент, запускаємо рекламу і будуємо стратегії для вашого бренду у Facebook, Instagram, LinkedIn та TikTok.',
+    images: ['https://mysmmexperts.com/images/meta.png'],
+  },
+};
 
 export default function Home() {
   return (
--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,4 +1,23 @@
+import type { Metadata } from 'next';
+
 import { Hero, PolicyContent } from './components';
+
+export const metadata: Metadata = {
+  title: 'Політика конфіденційності',
+  openGraph: {
+    title: 'Політика конфіденційності',
+    images: 'https://mysmmexperts.com/images/meta.png',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Політика конфіденційності',
+    images: ['https://mysmmexperts.com/images/meta.png'],
+  },
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default function PrivacyPolicy() {
   return (
