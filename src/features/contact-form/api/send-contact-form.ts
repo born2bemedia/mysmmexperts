d7a08730204a4af9cacf74a703a92634b75676a0
Daniil <dan.developer@born2be.media>
2025-06-19 16:50:38 +0300

    [feature/emails]: Email (#6)

---
+++ src/features/contact-form/api/send-contact-form.ts
@@ -0,0 +1,90 @@
+'use server';
+
+import { google } from 'googleapis';
+
+import { contactFormBody } from '@/features/email-letters/components/contact-form-body';
+
+import {
+  EMAIL_CLIENT_ID,
+  EMAIL_CLIENT_SECRET,
+  EMAIL_REFRESH_TOKEN,
+  EMAIL_USER,
+} from '@/shared/config/env';
+import { makeEmailBody } from '@/shared/lib/utils/email';
+
+import type { ContactSchema } from '../model/schema';
+
+export async function sendContactForm({
+  email,
+  phone,
+  fullName,
+  message,
+}: ContactSchema) {
+  try {
+    const OAuth2 = google.auth.OAuth2;
+    const oauth2Client = new OAuth2(
+      EMAIL_CLIENT_ID,
+      EMAIL_CLIENT_SECRET,
+      'https://developers.google.com/oauthplayground',
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token: EMAIL_REFRESH_TOKEN,
+    });
+
+    const accessToken = await oauth2Client.getAccessToken();
+
+    if (!accessToken.token) {
+      throw new Error('Failed to generate access token.');
+    }
+
+    const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+
+    const adminBody = makeEmailBody({
+      to: EMAIL_USER,
+      from: EMAIL_USER,
+      subject:
+        "Нове повідомлення з форми зворотнього зв'язку | mysmmexperts.com",
+      message: `<p><b>Ім'я:</b> ${fullName}</p>
+       <p><b>Email:</b> ${email}</p>
+       <p><b>Телефон:</b> ${phone}</p>
+       <p><b>Повідомлення:</b> ${message}</p>`,
+    });
+
+    const res = await gmail.users.messages.send({
+      userId: 'me',
+      requestBody: {
+        raw: adminBody,
+      },
+    });
+
+    const userBody = makeEmailBody({
+      to: email,
+      from: EMAIL_USER,
+      subject:
+        '=?utf-8?B?' +
+        Buffer.from('Дякуємо за звернення!').toString('base64') +
+        '?=',
+      message: contactFormBody({ username: fullName }),
+    });
+
+    await gmail.users.messages.send({
+      userId: 'me',
+      requestBody: { raw: userBody },
+    });
+
+    if (res.status !== 200) {
+      throw new Error(`Failed to send email. Status: ${res.status}`);
+    }
+
+    return { data: res.data, status: res.statusText };
+  } catch (err: unknown) {
+    console.error('Error order product:', err);
+
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    }
+
+    throw new Error('Unknown error occurred.');
+  }
+}
---
+++ src/features/email-letters/components/contact-form-body.tsx
@@ -0,0 +1,87 @@
+import { htmlTemplate } from './html-template';
+
+export const contactFormBody = ({ username }: { username: string }) => {
+  return htmlTemplate({
+    body: `
+      <div class="wrapper">
+        <header class="header">
+          <img style="width: 696px; height: 142px" src="https://images2.imgbox.com/ea/1f/kMVI4fAe_o.png" alt="full-logo" class="logo" />
+        </header>
+        <div class="main">
+          <p style="font-size: 30px; color: #323232;">Дякуємо за звернення!</p>
+          <p class="text">Доброго дня, ${username}</p>
+          <p class="text">Дякуємо за звернення. Ми вже працюємо над вашим запитом і незабаром з вами зв'яжемося.</p>
+          <p class="text">Якщо виникли питання, пишіть на gykovaon@gmail.com або телефонуйте: +380976930563.</p>
+          <p class="text">Гарного дня!</p>
+          <p class="text">З повагою, служба підтримки клієнтів</p>
+        </div>
+        <footer style="margin-top: 12px;">
+          <img style="width: 696px; height: 90px" src="https://images2.imgbox.com/92/38/sk18zlow_o.png" alt="full-logo" class="logoSm" />
+        </footer>
+      </div>
+    `,
+    style: `
+      .wrapper {
+        padding: 8px;
+        width: 696px;
+        margin: 0 auto;
+        background: #ffffff;
+      }
+      
+      .main {
+        margin-top: 12px;
+        padding: 40px;
+        border-radius: 12px;
+        background: #FDFDFD;
+      }
+      
+      .header {
+        overflow: hidden;
+      }
+      
+      .strong {
+        font-weight: 600;
+        color: #000;
+      }
+      
+      .title {
+        color: #000;
+        font-feature-settings: 'liga' off, 'clig' off;
+        font-size: 28px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: normal;
+      }
+       
+      .heading {
+        color: #000;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: normal;
+      }
+       
+      .text {
+        color: #323232;
+        font-size: 16px;
+      }
+      
+      .logo {
+        width: 159px;
+        height: 40px;
+      }
+      
+      .logoSm {
+        width: 157px;
+        height: 40px;
+      }
+      
+      .footer {
+        display: flex;
+        border-radius: 12px;
+        background: #FFE6E0;
+        padding: 20px;
+      }
+    `,
+  });
+};
